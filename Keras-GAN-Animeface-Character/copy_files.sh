753a2e012d7d7871bed377279735f498d8cd91bd
Injabie3 <injabie3@gmail.com>
2018-11-21 19:58:44 -0800

    Add script for copying files after training - Forgive me for this.

---
+++ Keras-GAN-Animeface-Character/copy_files.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Copy files to save after training
+
+mkdir data_to_move
+cp anim/frame_00000002.png data_to_move/
+
+cp anim/frame_00000001.png data_to_move/
+cp anim/frame_00001001.png data_to_move/
+cp anim/frame_00002001.png data_to_move/
+cp anim/frame_00003001.png data_to_move/
+cp anim/frame_00004001.png data_to_move/
+cp anim/frame_00005001.png data_to_move/
+cp anim/frame_00006001.png data_to_move/
+cp anim/frame_00007001.png data_to_move/
+cp anim/frame_00008001.png data_to_move/
+cp anim/frame_00008501.png data_to_move/
+cp anim/frame_00009001.png data_to_move/
+
+cp anim/frame_00000251.png data_to_move/
+cp anim/frame_00001251.png data_to_move/
+cp anim/frame_00002251.png data_to_move/
+cp anim/frame_00003251.png data_to_move/
+cp anim/frame_00004251.png data_to_move/
+cp anim/frame_00005251.png data_to_move/
+cp anim/frame_00006251.png data_to_move/
+cp anim/frame_00007251.png data_to_move/
+cp anim/frame_00008251.png data_to_move/
+cp anim/frame_00009251.png data_to_move/
+
+cp anim/frame_00000501.png data_to_move/
+cp anim/frame_00001501.png data_to_move/
+cp anim/frame_00002501.png data_to_move/
+cp anim/frame_00003501.png data_to_move/
+cp anim/frame_00004501.png data_to_move/
+cp anim/frame_00005501.png data_to_move/
+cp anim/frame_00006501.png data_to_move/
+cp anim/frame_00007501.png data_to_move/
+cp anim/frame_00008501.png data_to_move/
+cp anim/frame_00009501.png data_to_move/
+
+cp anim/frame_00000751.png data_to_move/
+cp anim/frame_00001751.png data_to_move/
+cp anim/frame_00002751.png data_to_move/
+cp anim/frame_00003751.png data_to_move/
+cp anim/frame_00004751.png data_to_move/
+cp anim/frame_00005751.png data_to_move/
+cp anim/frame_00006751.png data_to_move/
+cp anim/frame_00007751.png data_to_move/
+cp anim/frame_00008751.png data_to_move/
+cp anim/frame_00009751.png data_to_move/
+
+cp anim/frame_00009999.png data_to_move/
+
+cp loss.csv data_to_move/
